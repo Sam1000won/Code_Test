45f649fd637dbf1d5d3a4b4dfd317d60e6d9682a
jiughan <135206238+Sam1000won@users.noreply.github.com>
2024-05-02 10:34:40 +0900

    [Silver IV] Title: 행운의 수, Time: 568 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9575. 행운의 수/행운의 수.py
+++ 백준/Silver/9575. 행운의 수/행운의 수.py
@@ -1,5 +1,3 @@
-from itertools import product
-
 N = int(input())
 
 for _ in range(N):
@@ -9,12 +7,13 @@ for _ in range(N):
     lst_b = set(map(int, input().split()))
     c = int(input())
     lst_c = set(map(int, input().split()))
+    res = set()  # 결과를 중복없이 저장하기 위해 set으로 선언
 
-    res = set()
-    
-    for combination in product(lst_a, lst_b, lst_c):
-        total = sum(combination)
-        if all(char in {'5', '8'} for char in str(total)):
-            res.add(total)
+    for i in lst_a:
+        for j in lst_b:
+            for k in lst_c:
+                total = i + j + k
+                if all(char in {'5', '8'} for char in str(total)):
+                    res.add(total)  # 중복된 결과가 제거됨
 
     print(len(res))
