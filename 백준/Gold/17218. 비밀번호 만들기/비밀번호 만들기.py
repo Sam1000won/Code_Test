1109eeb72ec246fdec909428fb1cfdcc73ba4535
jiughan <135206238+Sam1000won@users.noreply.github.com>
2024-05-04 12:17:11 +0900

    [Gold IV] Title: 비밀번호 만들기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/17218. 비밀번호 만들기/비밀번호 만들기.py
+++ 백준/Gold/17218. 비밀번호 만들기/비밀번호 만들기.py
@@ -6,22 +6,25 @@ len_b = len(b)
 
 dp = [[0] * (len_b + 1) for _ in range(len_a + 1)]
 
-for i in range(len_a):
-    for j in range(len_b):
-        if a[i] == b[j]:
-            dp[i + 1][j + 1] = dp[i][j] + 1
+for i in range(1, len_a + 1):
+    for j in range(1, len_b + 1):
+        if a[i - 1] == b[j - 1]:
+            dp[i][j] = dp[i - 1][j - 1] + 1
         else:
-            dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j])
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
 
 res = ''
-while dp[len_a][len_b] != 0:
-    if dp[len_a][len_b] == dp[len_a - 1][len_b]:
-        len_a -= 1
-    elif dp[len_a][len_b] == dp[len_a][len_b - 1]:
-        len_b -= 1
-    else:
-        res += a[len_a - 1]
-        len_a -= 1
-        len_b -= 1
+i = len_a
+j = len_b
 
-print(res[::-1])
+while i > 0 and j > 0:
+    if a[i - 1] == b[j - 1]:
+        res = a[i - 1] + res
+        i -= 1
+        j -= 1
+    elif dp[i - 1][j] > dp[i][j - 1]:
+        i -= 1
+    else:
+        j -= 1
+
+print(res)
