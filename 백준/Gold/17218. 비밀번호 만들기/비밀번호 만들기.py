47568e924db6411575298c5e5bf6096701380014
jiughan <135206238+Sam1000won@users.noreply.github.com>
2024-05-04 12:18:47 +0900

    [Gold IV] Title: 비밀번호 만들기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/17218. 비밀번호 만들기/비밀번호 만들기.py
+++ 백준/Gold/17218. 비밀번호 만들기/비밀번호 만들기.py
@@ -1,30 +1,18 @@
+def longest_common_subsequence(a, b):
+    len_a = len(a)
+    len_b = len(b)
+    dp = [[""] * (len_b + 1) for _ in range(len_a + 1)]
+
+    for i in range(1, len_a + 1):
+        for j in range(1, len_b + 1):
+            if a[i - 1] == b[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + a[i - 1]
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1], key=len)
+
+    return dp[len_a][len_b]
+
 a = input()
 b = input()
 
-len_a = len(a)
-len_b = len(b)
-
-dp = [[0] * (len_b + 1) for _ in range(len_a + 1)]
-
-for i in range(1, len_a + 1):
-    for j in range(1, len_b + 1):
-        if a[i - 1] == b[j - 1]:
-            dp[i][j] = dp[i - 1][j - 1] + 1
-        else:
-            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
-
-res = ''
-i = len_a
-j = len_b
-
-while i > 0 and j > 0:
-    if a[i - 1] == b[j - 1]:
-        res = a[i - 1] + res
-        i -= 1
-        j -= 1
-    elif dp[i - 1][j] > dp[i][j - 1]:
-        i -= 1
-    else:
-        j -= 1
-
-print(res)
+print(longest_common_subsequence(a, b))
